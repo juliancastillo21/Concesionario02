19929f21b1baa1b80bb1cb0668f902abb9f1746e
juliancastillo21 <oscarjuliancastillo@gmail.com>
2024-05-11 12:04:10 -0500

    se estilo el Home

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,12 +1,9 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
-import { Text, TextInput, Button, List, Avatar, Card } from 'react-native-paper';
+import { Text, TextInput, Button, Card } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import Catalogue from './Catalogue';
 import FirebaseContext from '../../context/firebase/firebaseContext';
-
-
-
 
 const Home = () => {
   const navigation = useNavigation();
@@ -17,54 +14,82 @@ const Home = () => {
   }, []);
 
   return (
-    <View style={styles.contenedor}>
-      <Text style={styles.titulo}>Nuestro Catálogo</Text>
-      <TextInput
-        label="Buscar..."
-        style={styles.entradaBusqueda}
-      />
-      
+    <ScrollView>
+      <View style={styles.productContainer}>
+        <Text style={styles.titulo}>Nuestro Catálogo</Text>
+        <TextInput
+          label="Buscar..."
+          style={styles.entradaBusqueda}
+        />
         <View>
           {Catalogues.map((NuestroCatalogue, i) => {
             const { description, id, name, price, urlImagen } = NuestroCatalogue
-            return (
-              
-              <Fragment key={id}>
-                <Card>
-                  <Card.Cover source={{ uri:urlImagen }} />
+            return (           
+              <Fragment key={id} >
+                <Card style={styles.productImage}>
+                  <Card.Cover source={{ uri: urlImagen }}  />
+                  <Text style={styles.productName}>{name}</Text>
+                  <Text style={styles.productDescription}>{description}</Text>
+                  <Text style={styles.productPrice}>${price}</Text>
                 </Card>
-                <Text>{name}</Text>
-                <Text>{description}</Text>
-                <Text>{price}</Text>
               </Fragment>
-              
             )
           }
           )}
         </View>
-      
-      {navigation && (
-        <Button mode="contained" onPress={() => navigation.navigate('Offer')}>
-          Ir a la Siguiente Pantalla
-        </Button>
-      )}
-    </View>
+        {navigation && (
+          <Button mode="contained" onPress={() => navigation.navigate('Offer')}>
+            Ir a la Siguiente Pantalla
+          </Button>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  contenedor: {
+  productContainer: {
     flex: 1,
-    padding: 20,
+    padding: 20, 
+    backgroundColor: '#f5f5f5', 
   },
   titulo: {
     fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    fontWeight: 'bold', 
+    marginBottom: 10, 
   },
   entradaBusqueda: {
-    marginBottom: 20,
+    marginBottom: 10, 
   },
+  productImage: {
+    width: '100%', 
+    aspectRatio: 1, 
+    marginBottom: 10, 
+  },
+  productName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    marginTop: 15,
+    textAlign: 'center', 
+  },
+
+  productDescription: {
+    fontSize: 16, 
+    marginBottom: 5, 
+  },
+  productPrice: {
+    fontSize: 16, 
+    fontWeight: 'bold', 
+    color: '#00913f', 
+    textAlign: 'center', 
+  buttonContainer: { 
+    marginTop: 20, 
+  },
+  navigationButton: { 
+    backgroundColor: '#4CAF50', 
+  }},
 });
+
 
 export default Home;
